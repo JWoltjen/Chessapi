10d6f5e443338f724b619bd11412ac6c432a3e21
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-04-26 11:36:12 -0500

    instantiate initial position

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import ChessBoard from 'chessboardjsx'
 function App() {
   return (
     <div className="App">
-     
+      <ChessBoard position='start'/>
     </div>
   );
 }
