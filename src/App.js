79ef1600cb1f11509ad2ec9c1edc0797604d9beb
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-04-26 11:37:46 -0500

    add styling to container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import ChessBoard from 'chessboardjsx'
+import Chess from 'chess.js'
+const container = {
+  marginTop: '2rem', 
+  display: 'flex', 
+  justifyContent: 'space-around', 
+  alignItem: 'center', 
+}
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={container}>
       <ChessBoard position='start'/>
     </div>
   );
